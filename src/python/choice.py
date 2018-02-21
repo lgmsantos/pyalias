98b966fd65a34d0bbb75ca077ca56b85c7314831
lgmsantos <lgmsantos@gmail.com>
2018-02-21 15:31:19 -0300

    Ignore rules added.  - ignore 'data' dir  - ignore vim swap files  - ignore generated .c files

--- src/python/choice.py
+++ src/python/choice.py
@@ -1,3 +1,11 @@
+"""Execute the specified method for random choice with arbitrary probability distribution and output information about execution
+
+usage: 
+    choice header
+    choice (alias|binsearch-old|binsearch-fixed) <p_file> <sample_size>
+"""
+
+from docopt import docopt
 import sys
 import numpy as np
 import alias as al
@@ -6,26 +14,25 @@ import time
 BUFFER_SIZE=1000
 
 if __name__ == '__main__':
+    args = docopt(__doc__)
     
-    args = sys.argv[1:]
-    method_name = args[0]
-
-    if method_name == 'header':
+    if args['header']:
         print('method,distribution_size,sample_size,init_time,time,mean_error,std_error,min_error,max_error')
         sys.exit(0)
 
-    p_path = args[1]
-    p = np.fromfile(p_path)
+    p = np.fromfile(args['<p_file>'])
     n = len(p)
     values = np.arange(n)
-    sample_size = int(args[2])
+    sample_size = int(args['<sample_size>'])
 
-    if method_name == 'binsearch-old':
+    if args['binsearch-old']:
+        method_name = 'binsearch-old'
         init_time = 0
         def choice(k): 
             return np.random.choice(values, p=p, size=k)
 
-    elif method_name == 'binsearch-fixed':
+    elif args['binsearch-fixed']:
+        method_name = 'binsearch-fixed'
         start = time.perf_counter()
         dist = np.random.pdist.wrap(p)
         dist.cumsum
@@ -34,7 +41,8 @@ if __name__ == '__main__':
         def choice(k): 
             return np.random.choice(values, p=dist, size=k)
 
-    elif method_name == 'alias':
+    elif args['alias']:
+        method_name = 'alias'
         start = time.perf_counter()
         table = al.aliastable(p)
         end = time.perf_counter()
@@ -43,7 +51,7 @@ if __name__ == '__main__':
             return al.choice(table, values, k)
         
     else:
-        print('invalid method:', method_name, file=sys.stderr)
+        print('invalid args', file=sys.stderr)
         print('available options: alias, binsearch-old, binsearch-fixed', file=sys.stderr)
         sys.exit(1)
 
--- src/python/random_dist.py
+++ src/python/random_dist.py
@@ -1,24 +1,30 @@
+"""Generate a random discrete probability distribution of size <n> and save it to <file>.
+
+usage:
+    random_dist -h
+    random_dist <n> <file>"""
+
 from os.path import exists
 import numpy as np
-import sys 
-
-def print_usage():
-    print('random_dist <N> <PATH>')
-    print('\tgenerate a random discrete probabilitiy distribution with <N> elements and save it to <PATH>')
+import docopt
+from sys import stderr, exit
 
 if __name__ == '__main__':
+    args = docopt.docopt(__doc__)
 
-    args = sys.argv[1:]
-    if len(args) != 2:
-        print_usage()
-        sys.exit(1)
+    try:
+        n = int(args['<n>'])
+    except ValueError:
+        n = 0
 
-    n = int(args[0])
-    path = args[1]
+    if n <= 0:
+        print('<n> must be a positive integer', file=stderr)
+        exit(1)
 
+    path = args['<file>']
     if exists(path):
-        print('file {} exists. aborting.', file=sys.stderr)
-        sys.exit(1)
+        print('file "{}" exists, aborting.'.format(path), file=stderr)
+        exit(1)
 
     p = np.random.random(n)
     p = p/np.sum(p)
