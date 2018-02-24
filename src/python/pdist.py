8c8a9d82871910b9b772c326447ebeace4859d75
lgmsantos <lgmsantos@gmail.com>
2018-02-24 20:02:13 -0300

    "random_dist" now equivalent to "pdist random"  - subcommands added: linear, quad and exp

--- src/python/pdist.py
+++ src/python/pdist.py
@@ -1,8 +1,8 @@
 """Generate a random discrete probability distribution of size <n> and save it to <file>.
 
 usage:
-    random_dist -h
-    random_dist <n> <file>"""
+    pdist -h
+    pdist (random|linear|quad|exp) <n> <file>"""
 
 from os.path import exists
 import numpy as np
@@ -26,6 +26,22 @@ if __name__ == '__main__':
         print('file "{}" exists, aborting.'.format(path), file=stderr)
         exit(1)
 
-    p = np.random.random(n)
+    if args['random']:
+        p = np.random.random(n)
+
+    elif args['linear']:
+        p = np.arange(n, dtype='d')
+
+    elif args['quad']:
+        p = np.arange(n, dtype='d') ** 2
+
+    elif args['exp']:
+        p = 2 ** np.arange(n, dtype='d')
+
+    else:
+        print('illegal subcommand', file=sys.stderr)
+        sys.exit(1)
+
+    np.random.shuffle(p)
     p = p/np.sum(p)
     p.tofile(path)
